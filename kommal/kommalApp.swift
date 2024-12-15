07c26e90f5c3d0203b00fafd5961556510282b70
ghadasaw21 <academyghadaphone@icloud.com>
2024-12-15 15:32:09 +0300

    Initial Commit

---
+++ kommal/kommalApp.swift
@@ -0,0 +1,17 @@
+//
+//  kommalApp.swift
+//  kommal
+//
+//  Created by Ghada al ajmi on 14/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct kommalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
