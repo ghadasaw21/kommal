0e7c9a50f8f99528713972e43ef5961d8aa5e6dd
hailahS <hailahalsaudi@outlook.sa>
2024-12-16 17:08:20 +0300

    c

--- kommal/View/Splash.swift
+++ kommal/View/Splash.swift
@@ -5,4 +5,36 @@
 //  Created by Ghada al ajmi on 15/06/1446 AH.
 //
 
-import Foundation
+import SwiftUI
+
+struct Splash: View {
+    var body: some View {
+        
+        ZStack{
+            LinearGradient(gradient: .init(colors: [.blu1,.blu2,.blu3,.blu4]), startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+            
+           
+            VStack{
+                Circle()
+                    .fill(Color.circle.opacity(0.19)) // شفافية 50%
+                                .frame(width: 562, height: 612).offset(y:-225) // حجم الدائرة
+                            
+                
+            }
+            ZStack{
+                Circle()
+                    .fill(Color.circle.opacity(0.19)) // شفافية 50%
+                                .frame(width: 562, height: 612).offset(y:200)// حجم الدائرة
+                            
+            }
+            
+        }
+       
+        
+        
+        
+        
+    }}
+#Preview {
+    Splash()
+}
